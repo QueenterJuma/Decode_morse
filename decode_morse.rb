247712fd347879e9ab49c9fe710f5fcd37a382a0
QueenterJuma <jumahqueenter@gmail.com>
2023-05-11 01:14:27 +0300

    Add decode_morse.rb file

---
+++ decode_morse.rb
@@ -0,0 +1,48 @@
+class MorseCodeDecoder
+  MORSE_CODE = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }.freeze
+
+  def self.decode_char(code)
+    MORSE_CODE[code]
+  end
+
+  def self.decode_word(word)
+    decoded_word = ''
+    characters = word.split
+    characters.each { |char| decoded_word << decode_char(char) }
+    decoded_word
+  end
+
+  def self.decode(message)
+    decoded_message = ''
+    words = message.split
+    words.each { |word| decoded_message << decode_word(word) << ' ' }
+    decoded_message.strip
+  end
+end
